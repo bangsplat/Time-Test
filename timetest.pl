300b05fac1e286e9bc535d04720b4003f680e86c
Bangsplat <theron@dankerton.net>
2013-06-20 19:36:57 -0700

    added comment

--- timetest.pl
+++ timetest.pl
@@ -5,7 +5,7 @@ use Time::localtime;
 
 my @MONTHS = qw( 01 02 03 04 05 06 07 08 09 10 11 12 );
 my @DAYS = qw( 00 01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 );
-my $ONE_DAY = 24 * 60 * 60;
+my $ONE_DAY = 24 * 60 * 60;		# 86400 seconds in a day.  Usually
 
 my $this_year = localtime()->year + 1900;
 my $this_month = $MONTHS[localtime()->mon];
